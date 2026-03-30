9d364fa8f0d195b74ff2ee09cfef3c146a2d014f
Laura <lauxavi1357@gmail.com>
2026-03-30 17:06:20 -0300

    Criando repositório

---
+++ ProjetoAPIEnd/Models/Endereco.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAPIEnd.Models
+{
+    public class Endereco
+    {
+        [Display(Name = "Código", Description = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "CEP")]
+        public string CEP
+    }
+}
---
+++ ProjetoAPIEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoAPIEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
